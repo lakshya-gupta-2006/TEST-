2e239f4b1474accb6b894e7fb68d47a8025fd2db
Lakshya Gupta <guptaalakshy@gmail.com>
2025-09-08 07:21:52 +0530

    all _operators.py

---
+++ _operators.py
@@ -0,0 +1,17 @@
+a=int(input("Enter a : "))
+b=float(input("Enter b : "))
+c = a+b
+d = a-b
+e = a*b    #multiplication
+f = a/b  #division ka ans
+g = a%b #reminder with 1 step od division (without 0. or decimal ).
+print("sum of a+b")
+print(c)
+print("subtraction of a-b")
+print(d)
+print("multiplication of a*b")
+print(e)
+print("division of a & b")
+print(f)
+print("reminder of a/b")
+print(g)
